5f8b79faa633017b1fab9c3693aa92f0e9ffb717
CaptainCthulhu <theonegod@gmail.com>
2018-12-02 08:05:56 -0700

    Night 2

---
+++ Advent of Code/2018/2-1.py
@@ -0,0 +1,23 @@
+import sys
+from storage import Question2
+
+var = Question2.Part1
+
+two = 0
+three = 0
+
+
+for i in var:
+    letters = {}
+    for x in i:        
+        letters[x] = (letters.get(x) or 0) + 1
+    
+    if len([x for x in letters.values() if x == 3]) > 0:
+        three += 1
+    if len([x for x in letters.values() if x == 2]) > 0:
+        two += 1
+
+print (two * three)
+
+
+
---
+++ Advent of Code/2018/2-2.py
@@ -0,0 +1,31 @@
+import sys
+from storage import Question2 
+
+import sys
+from storage import Question2
+
+var = Question2.Part1
+
+def OffByOne(word1, word2):    
+    items = ''
+    goal = len(word1) - 1
+    for x in range(len(word1)):
+        if word1[x] == word2[x]:
+            items += word1[x]
+    if len(items) == goal:
+        return items
+    return False
+    
+
+
+
+for i in range(len(var) - 1):
+    for x in range(i, len(var)):
+        result = OffByOne(var[i], var[x])
+        if result:
+            print(result)
+            break
+
+
+
+    

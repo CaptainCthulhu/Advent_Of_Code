2b33d1ecdce1bbf59b766811afdf085bfe2747d4
Ian Dobson <theonegod@gmail.com>
2018-12-09 07:47:38 -0700

    Night 9

---
+++ Advent of Code/2018/Problem9/Problem9/Marbel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem9
+{
+    class Marbel
+    {
+        public int Value;
+        public Marbel Left;
+        public Marbel Right;
+
+        public Marbel(int value)
+        {
+            this.Value = value;
+        }
+
+
+        public void SetLeftRight(Marbel left, Marbel right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+
+        public static Marbel Add(int value, Marbel currentMarbel)
+        {
+            Marbel newMarbel = new Marbel(value);
+            newMarbel.Left = currentMarbel;
+            newMarbel.Right = newMarbel.Left.Right;
+            newMarbel.Right.Left = newMarbel;            
+            newMarbel.Left.Right = newMarbel;
+            return newMarbel;
+        }
+
+        public static Marbel Remove(Marbel currentMarbel)
+        {
+            currentMarbel.Left.Right = currentMarbel.Right;
+            currentMarbel.Right.Left = currentMarbel.Left;
+            return currentMarbel.Right;
+        }
+
+        public static Marbel GoLeft(Marbel marbel)
+        {
+            return marbel.Left;
+        }
+
+        public static Marbel GoRight(Marbel marbel)
+        {
+            return marbel.Right;
+        }
+
+    }
+}
---
+++ Advent of Code/2018/Problem9/Problem9/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem9
+{
+    class Program
+    {
+        
+        static long[] playerScores = new long[459];        
+        static int turn = 0;
+        static int goal = 7210300;
+
+        static void Main(string[] args)
+        {
+            PlayGame();
+            Console.WriteLine(playerScores.Max());
+            Console.ReadKey();
+        }
+
+
+        private static void PlayGame()
+        {
+            //Initial Marbel
+            Marbel marbel = new Marbel(0);
+            marbel.SetLeftRight(marbel, marbel);
+            Marbel currentMarbel = marbel;
+            
+            while (turn < goal)
+            {
+                turn++;
+                if (turn % 23 == 0)
+                {                    
+                    playerScores[turn % playerScores.Count()] += turn;
+                    for (int i = 0; i < 7; i++)
+                    {
+                        currentMarbel = Marbel.GoLeft(currentMarbel);                          
+                    }
+                    playerScores[turn % playerScores.Count()] += currentMarbel.Value;
+                    currentMarbel = Marbel.Remove(currentMarbel);
+                }
+                else
+                {                    
+                    currentMarbel = Marbel.GoRight(currentMarbel);                                              
+                    currentMarbel = Marbel.Add(turn, currentMarbel);
+                }                
+            }
+        }
+    }
+}
